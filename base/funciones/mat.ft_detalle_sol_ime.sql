d97ac0c8132cb22b73687918cd0378a07c5178e4
Jaime Rivera <rensi4rn@gmail.com>
2017-01-08 13:16:59 -0400

    funcion ime de detalles

--- base/funciones/mat.ft_detalle_sol_ime.sql
+++ base/funciones/mat.ft_detalle_sol_ime.sql
@@ -1,16 +1,24 @@
+CREATE OR REPLACE FUNCTION mat.ft_detalle_sol_ime (
+  p_administrador integer,
+  p_id_usuario integer,
+  p_tabla varchar,
+  p_transaccion varchar
+)
+RETURNS varchar AS
+$body$
 /**************************************************************************
  SISTEMA:		Sistema Gestión de Materiales
  FUNCION: 		mat.ft_detalle_sol_ime
  DESCRIPCION:   Funcion que gestiona las operaciones basicas (inserciones, modificaciones, eliminaciones de la tabla 'mat.tdetalle_sol'
  AUTOR: 		 (admin)
  FECHA:	        23-12-2016 13:13:01
- COMENTARIOS:	
+ COMENTARIOS:
 ***************************************************************************
  HISTORIAL DE MODIFICACIONES:
 
- DESCRIPCION:	
- AUTOR:			
- FECHA:		
+ DESCRIPCION:
+ AUTOR:
+ FECHA:
 ***************************************************************************/
 
 DECLARE
@@ -22,21 +30,21 @@ DECLARE
 	v_nombre_funcion        text;
 	v_mensaje_error         text;
 	v_id_detalle	integer;
-			    
+
 BEGIN
 
     v_nombre_funcion = 'mat.ft_detalle_sol_ime';
     v_parametros = pxp.f_get_record(p_tabla);
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'MAT_DET_INS'
  	#DESCRIPCION:	Insercion de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		23-12-2016 13:13:01
 	***********************************/
 
 	if(p_transaccion='MAT_DET_INS')then
-					
+
         begin
         	--Sentencia de la insercion
         	insert into mat.tdetalle_sol(
@@ -74,7 +82,7 @@ BEGIN
 			null,
 			null)RETURNING id_detalle into v_id_detalle;
 			--Definicion de la respuesta
-			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Detalle_sol almacenado(a) con exito (id_detalle'||v_id_detalle||')'); 
+			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Detalle_sol almacenado(a) con exito (id_detalle'||v_id_detalle||')');
             v_resp = pxp.f_agrega_clave(v_resp,'id_detalle',v_id_detalle::varchar);
 
             --Devuelve la respuesta
@@ -82,10 +90,10 @@ BEGIN
 
 		end;
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'MAT_DET_MOD'
  	#DESCRIPCION:	Modificacion de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		23-12-2016 13:13:01
 	***********************************/
 
@@ -108,20 +116,20 @@ BEGIN
 			id_usuario_ai = v_parametros._id_usuario_ai,
 			usuario_ai = v_parametros._nombre_usuario_ai
 			where id_detalle=v_parametros.id_detalle;
-               
+
 			--Definicion de la respuesta
-            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Detalle_sol modificado(a)'); 
+            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Detalle_sol modificado(a)');
             v_resp = pxp.f_agrega_clave(v_resp,'id_detalle',v_parametros.id_detalle::varchar);
-               
+
             --Devuelve la respuesta
             return v_resp;
-            
+
 		end;
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'MAT_DET_ELI'
  	#DESCRIPCION:	Eliminacion de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		23-12-2016 13:13:01
 	***********************************/
 
@@ -131,29 +139,35 @@ BEGIN
 			--Sentencia de la eliminacion
 			delete from mat.tdetalle_sol
             where id_detalle=v_parametros.id_detalle;
-               
+
             --Definicion de la respuesta
-            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Detalle_sol eliminado(a)'); 
+            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Detalle_sol eliminado(a)');
             v_resp = pxp.f_agrega_clave(v_resp,'id_detalle',v_parametros.id_detalle::varchar);
-              
+
             --Devuelve la respuesta
             return v_resp;
 
 		end;
-         
+
 	else
-     
+
     	raise exception 'Transaccion inexistente: %',p_transaccion;
 
 	end if;
 
 EXCEPTION
-				
+
 	WHEN OTHERS THEN
 		v_resp='';
 		v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
 		v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
 		v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
 		raise exception '%',v_resp;
-				        
+
 END;
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
+COST 100;
