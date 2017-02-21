24e1eb34022bf8a0993adf4d86e4364578dc5c1e
Jaime Rivera <rensi4rn@gmail.com>
2017-02-21 11:17:49 -0400

    modificaciones

--- vista/solicitud/Solicitud.php
+++ vista/solicitud/Solicitud.php
@@ -330,10 +330,10 @@ header("content-type: text/javascript; charset=UTF-8");
                     renderer:function (value,p,record){
 
                         var dias = record.data.control_fecha;
-                        if (dias >= 2 && dias <= 10) {
+                        if (dias >= 6 && dias <= 10) {
                             return String.format('<div ext:qtip="Optimo"><b><font color="green">{0}</font></b><br></div>', value?value.dateFormat('d/m/Y'):'');
                         }
-                        else if(dias >=4  && dias <= 6){
+                        else if(dias >= 2 && dias <= 4){
                             return String.format('<div ext:qtip="Critico"><b><font color="orange">{0}</font></b><br></div>', value?value.dateFormat('d/m/Y'):'');
                         }else if(dias = -1){
                             return String.format('<div ext:qtip="malo"><b><font color="red">{0}</font></b><br></div>', value?value.dateFormat('d/m/Y'):'');
