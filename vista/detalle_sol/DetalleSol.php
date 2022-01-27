829bd8e7d480bb8f9796a74ce8ee4558febeab9b
ismael2019 <irva2008_@hotmail.com>
2022-01-27 15:55:25 -0400

    Quitando boton new

--- vista/detalle_sol/DetalleSol.php
+++ vista/detalle_sol/DetalleSol.php
@@ -967,7 +967,7 @@ Phx.vista.DetalleSol=Ext.extend(Phx.gridInterfaz,{
 	bdel:true,
 	bsave:false,
 	btest: false,
-  bnew: true,
+  bnew: false,
 
         onReloadPage: function (m) {
             this.maestro = m;
