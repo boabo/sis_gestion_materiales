122467579ba1c9e7bbc973e52dda27f9be0c046d
ismael2019 <irva2008_@hotmail.com>
2022-06-10 20:14:07 -0400

    Comentando momentaneamente

--- control/ACTSolicitud.php
+++ control/ACTSolicitud.php
@@ -790,7 +790,7 @@ class ACTSolicitud extends ACTbase{
            }
 
            if ($datosDetalle[$i]["condicion"] == '') {
-             throw new Exception("La Condicion Item: ".$item." no puede ser vacio favor verificar el detalle de la cotización adjudicada");
+            // throw new Exception("La Condicion Item: ".$item." no puede ser vacio favor verificar el detalle de la cotización adjudicada");
            }
 
            if ($datosDetalle[$i]["fechaentrega"] == '') {
